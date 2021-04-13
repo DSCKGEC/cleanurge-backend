934f4145c1aa72b8bf3485a54bbe3e5d3e27d63f
singhayushh <coding.ayush@gmail.com>
2021-04-13 19:58:38 +0530

    added: helper func to generate random string

---
+++ utils/helper.js
@@ -0,0 +1,17 @@
+/* ------------ Helper Functions ----------- */
+
+const generateCode = (length) => {
+    var result = '#';
+    var characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(
+            Math.floor(Math.random() * charactersLength)
+        );
+    }
+    return result;
+};
+
+module.exports = {
+    generateCode,
+};
