cc1aa40e6806c186c2f98d354dec5d714a264e5b
singhayushh <coding.ayush@gmail.com>
2021-05-22 16:11:55 +0530

    test route to fetch coordinates from IP

--- utils/fetch_ip.js
+++ utils/fetch_ip.js
@@ -3,31 +3,7 @@ var geoip = require('geoip-lite');
 const fetchIP = async (req, res) => {
     const client_ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     const getgeo = geoip.lookup(client_ip);
-    let country = 'unknown';
-    let geo = [-1, -1];
-    let address = 'unknown';
-    let province = 'unknown';
-
-    var client_location = {
-        country,
-        geo,
-        address,
-        province,
-    };
-    if (getgeo) {
-        country = getgeo.country;
-        geo = getgeo.ll;
-        address = getgeo.city;
-        province = getgeo.region;
-
-        client_location = {
-            country,
-            geo,
-            address,
-            province,
-        };
-    }
-    res.status(200).json(client_location);
+    res.status(200).json(getgeo);
 };
 
 module.exports = fetchIP;
